7ee0f3d907e4aca0593bdfb28db32e9916d96e16
nikassaakadze <nikasaakade2@gmail.com>
2020-11-23 04:38:28 -0800

    dwqD

--- js/gsap.js
+++ js/gsap.js
@@ -4,4 +4,4 @@ const banText = document.querySelector(".banner-text")
 const tI = new TimelineMax()
 
 tI.fromTo(hero, 1.5, {height: "0px"}, {height: "495px"})
-.fromTo(banText, 1, {lineHeight: 0, opacity: 0}, {lineHeight: 1.5, opacity: 1})
+.fromTo(banText, 1, {lineHeight: 0, opacity: 0}, {lineHeight: 1.4, opacity: 1})
