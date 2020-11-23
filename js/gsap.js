c1348a11be051fca91c1d36a935938daa7a41aed
nikassaakadze <nikasaakade2@gmail.com>
2020-11-23 03:24:52 -0800

    travel

---
+++ js/gsap.js
@@ -0,0 +1,7 @@
+const hero = document.querySelector(".hero")
+const banText = document.querySelector(".banner-text")
+
+const tI = new TimelineMax()
+
+tI.fromTo(hero, 1.5, {height: "0px"}, {height: "495px"})
+.fromTo(banText, 1, {lineHeight: 0, opacity: 0}, {lineHeight: 1.5, opacity: 1})
